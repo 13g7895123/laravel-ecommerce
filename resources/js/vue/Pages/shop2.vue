fce89e7a095c08d8805648ba41ec54b79343d5e1
Jarvis <13g7895123@gmail.com>
2023-02-26 20:23:41 +0800

    add products

---
+++ resources/js/vue/Pages/shop2.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="grid grid-cols-7 gap-1">
+        <v-card
+            v-for="product in products"
+            max-width="150"
+            variant="outlined"
+            >
+            <v-card-item>
+                <div>{{ product.name }}</div>
+                <v-img :src="product.img_url"></v-img>
+            </v-card-item>
+            <v-card-actions>
+                <v-btn variant="outlined">
+                    加入購物車
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            products: [
+                {
+                    name: '耳機',
+                    img_url: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                },
+                {
+                    name: '球鞋',
+                    img_url: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+                {
+                    name: '手錶',
+                    img_url: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80'
+                },
+            ],
+        }        
+    }
+}
+
+</script>
